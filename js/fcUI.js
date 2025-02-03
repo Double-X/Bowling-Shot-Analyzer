f9a010ec77e680faba3a46f561843c43609c62bd
Double-X <lkllun@yahoo.com.hk>
2025-02-03 20:07:40 +0800

    Add files via upload

--- js/fcUI.js
+++ js/fcUI.js
@@ -3,28 +3,23 @@ const FCUI = {
     loadGraph: async fileInput => {
         const onFail = FCUI._onLoadFail, files_ = fileInput.srcElement.files;
         if (!files_) return onFail("No graph to be loaded");
-        const file = files_[0];
-        const dataURL = await FMGraphVideo.dataURL(file).catch(onFail);
-        console.log(dataURL)
+        const dataURL = await FMGraphVideo.dataURL(files_[0]).catch(onFail);
+        FVGraph.load(dataURL);
     },
     loadVideo: async fileInput => {
         const onFail = FCUI._onLoadFail, files_ = fileInput.srcElement.files;
         if (!files_) return onFail("No video to be loaded");
         const file = files_[0];
         const arrayBuffer = await FMGraphVideo.arrayBuffer(file).catch(onFail);
-        const videoPlayer = document.getElementById("videoPlayer");
-        videoPlayer.src = URL.createObjectURL(new Blob([arrayBuffer]));
+        FVVideoPlayer.load(arrayBuffer);
     },
     _onLoadFail: message => {
         alert(message);
         console.error(message);
     },
     setCurrentFrame: ({ target }) => {
-        const videoPlayer = document.getElementById("videoPlayer");
         const frameRate = document.getElementById("frameRate").value;
-        const { currentTime } = videoPlayer, currentFrame = target.value;
-        const timeOffset = currentFrame * 1.0 / frameRate;
-        videoPlayer.currentTime = Math.floor(currentTime) + timeOffset;
+        FVVideoPlayer.setCurrentFrame(frameRate, target.value);
     },
     updateCurrentFrame: ({ target }) => {
         const { currentTime } = target;
@@ -41,7 +36,7 @@ const FCUI = {
         FMCP.IsGetRGB[type] = true;
         FVVideoPlayer.showAnalyzer();
     },
-    tryGetLaneCornerPosition: (horizontal, vertical) => {
+    tryGetLaneCornerXY: (horizontal, vertical) => {
         FMCP.clearLaneCornerFlags();
         FMCP.IsGetLaneCorner[horizontal][vertical] = true;
         FVVideoPlayer.showAnalyzer();
@@ -49,8 +44,9 @@ const FCUI = {
     clickVideoAnalyzer: ({ pageX, pageY }) => {
         const type_ = FMCP.rgbType_();
         if (type_) return FCUI._getRGB(pageX, pageY, type_);
-        const [horizontal_, vertical_] = FMCP.laneCornerPosition_();
-        if (horizontal_ && vertical_) FCUI._getLaneCornerPosition(pageX, pageY, horizontal_, vertical_);
+        const [horizontal_, vertical_] = FMCP.laneCornerXY_();
+        if (!horizontal_ || !vertical_) return;
+        FCUI._getLaneCornerXY(pageX, pageY, horizontal_, vertical_);
     },
     _getRGB: (pageX, pageY, type) => {
         const rgb = FVVideoPlayer.rgb(pageX, pageY);
@@ -60,11 +56,11 @@ const FCUI = {
         FMCP.clearRGBFlags();
         FVVideoPlayer.hideAnalyzer();
     },
-    _getLaneCornerPosition: (pageX, pageY, horizontal, vertical) => {
-        const xy = FMCP.LaneCornerPosition[horizontal][vertical];
-        [xy.x, xy.y] = FVVideoPlayer.laneCornerPosition(pageX, pageY);
+    _getLaneCornerXY: (pageX, pageY, horizontal, vertical) => {
+        const xy = FMCP.LaneCornerXY[horizontal][vertical];
+        [xy.x, xy.y] = FVVideoPlayer.laneCornerXY(pageX, pageY);
         const text = document.getElementById(`corner${horizontal}${vertical}`);
-        text.innerHTML = `x - ${xy.x} / y - ${xy.y}`;
+        text.innerHTML = `x${xy.x}y${xy.y}`;
         FMCP.clearLaneCornerFlags();
         FVVideoPlayer.hideAnalyzer();
     }
