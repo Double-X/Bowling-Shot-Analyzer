572089404004c8c96ba2c54e9728e9fe551ed1a3
Double-X <lkllun@yahoo.com.hk>
2025-04-21 23:40:55 +0800

    Delete js/fm/fmVideo.js

--- js/fm/fmVideo.js
+++
@@ -1,9 +0,0 @@
-const FMVideo = {
-    arrayBuffer: file => new Promise((resolve, reject) => {
-        const fileReader = new FileReader();
-        fileReader.onerror = reject;
-        fileReader.onload = FMVideo._onLoadOk.bind(this, resolve, fileReader);
-        fileReader.readAsArrayBuffer(file);
-    }),
-    _onLoadOk: (callback, fileReader) => callback(fileReader.result)
-};
